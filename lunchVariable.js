d06f0d8a8d2989b510adb3594dbc46c4784e4cdd
Melissa <meliarneau@gmail.com>
2018-02-08 18:49:13 +0000

    Create JS file for Lunch Variable exercise

---
+++ lunchVariable.js
@@ -0,0 +1,5 @@
+lunch = 'lasagna'
+sizeOfParty = 4
+totalCost = 32.95
+averageCost = totalCost / sizeOfParty
+message = 'Your lunch of ' + lunch + ' for ' + sizeOfParty + ' people will cost you £' + totalCost + ' or £' + averageCost.toFixed(2) + ' each.'
