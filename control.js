fe951700f26eb937912a1dca460bf0d65e6324d9
Melissa <meliarneau@gmail.com>
2018-02-19 19:03:34 +0000

    Replace while/do while loops with for loop

--- control.js
+++ control.js
@@ -5,16 +5,14 @@ var getDieRoll = function(dieSize) {
 
 var roll = getDieRoll(6);
 
-while(roll < 4) {
-	console.log(roll);
-	roll = getDieRoll(6);
+for(var i = 0; i < 10; i+= 1) {
+	if (roll >= 4){
+		console.log('*');
+	}
+	else if (roll <= 3) {
+		console.log('-');
+	}
 }
-
-do {
-	roll = getDieRoll(6);
-	console.log(roll);
-} while(roll > 4);
-
 
 if(roll >= 3 && roll <= 5){
 	console.log("Great roll!");
