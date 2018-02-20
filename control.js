45d6812c6f5599a6a855dd49a448330c2fd874b0
Melissa <meliarneau@gmail.com>
2018-02-20 10:06:36 +0000

    Add arrays

--- control.js
+++ control.js
@@ -1,7 +1,15 @@
 var getDieRoll = function(dieSize) {
-	var result = Math.ceil(dieSize * Math.random())
+	var result = Math.ceil(dieSize * Math.random());
 	return result;
 };
+
+var things = [1, 2, 3, "pizza"];
+
+console.log(things[3]);
+
+for(var i =0; i < things.length; i++){
+	console.log(things[i]);
+}
 
 var roll = getDieRoll(6);
 
